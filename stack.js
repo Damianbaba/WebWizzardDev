9e91205b2cb32fcf415285f66c983178fe43c61c
Damian Mroczek <damianmroczek@yahoo.com>
2024-03-04 21:32:46 +0100

    skills section js

---
+++ stack.js
@@ -0,0 +1,22 @@
+import './main.scss';
+
+const scrollers = document.querySelectorAll('.stack');
+
+
+function addAnimation() {
+	scrollers.forEach((scroller) => {
+		// add data-animated="true" to every `.scroller` on the page
+		scroller.setAttribute("data-animated", true);
+
+
+		const scrollerInnerWrapper = scroller.querySelector('.stack__inner-wrappper');
+		const scrollerInner = scroller.querySelector('.stack__inner');
+		const scrollerInnerContent = Array.from(scrollerInner.children);
+
+		const dupilcatedItem = scrollerInner.cloneNode(true)
+		dupilcatedItem.setAttribute('aria-hidden', true)
+		scrollerInner.parentElement.appendChild(dupilcatedItem)
+	})
+}
+
+addAnimation()
